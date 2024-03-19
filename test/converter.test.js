d14babdcd31ac536319a4f93a5385d9723142a1a
Tytianiuk Artem <tytianiuk@gmail.com>
2024-03-19 02:04:44 +0200

    Fix tests for new ability of selection mode

--- test/converter.test.js
+++ test/converter.test.js
@@ -3,11 +3,11 @@ const { STRINGS } = require('../strings')
 
 describe('Checking paragraphs:', () => {
     test('with one paragraph', () => {
-        expect(convert('paragraph')).toBe('<p>paragraph</p>')
+        expect(convert('paragraph', 'html')).toBe('<p>paragraph</p>')
     })
 
     test('with two paragraphs', () => {
-        expect(convert('paragraph\r\n\r\nparagraph')).toBe(
+        expect(convert('paragraph\r\n\r\nparagraph', 'html')).toBe(
             '<p>paragraph</p>\r\n<p>paragraph</p>',
         )
     })
@@ -15,59 +15,67 @@ describe('Checking paragraphs:', () => {
 
 describe('Checking bold tags:', () => {
     test('with common tags', () => {
-        expect(convert('**bold**')).toBe('<p><b>bold</b></p>')
+        expect(convert('**bold**', 'html')).toBe('<p><b>bold</b></p>')
     })
 
     test('with just symbol', () => {
-        expect(convert('**')).toBe('<p>**</p>')
+        expect(convert('**', 'html')).toBe('<p>**</p>')
     })
 
     test('with just symbol in tags', () => {
-        expect(convert('**bold ** bold**')).toBe('<p><b>bold ** bold</b></p>')
+        expect(convert('**bold ** bold**', 'html')).toBe(
+            '<p><b>bold ** bold</b></p>',
+        )
     })
 
     test('with just symbol in word in tags', () => {
-        expect(convert('**bold**bold**')).toBe('<p><b>bold**bold</b></p>')
+        expect(convert('**bold**bold**', 'html')).toBe(
+            '<p><b>bold**bold</b></p>',
+        )
     })
 })
 
 describe('Checking italic tags:', () => {
     test('with common tags', () => {
-        expect(convert('_italic_')).toBe('<p><i>italic</i></p>')
+        expect(convert('_italic_', 'html')).toBe('<p><i>italic</i></p>')
     })
 
     test('with just symbol', () => {
-        expect(convert('_')).toBe('<p>_</p>')
+        expect(convert('_', 'html')).toBe('<p>_</p>')
     })
 
     test('with just symbol in tags', () => {
-        expect(convert('_italic _ italic_')).toBe(
+        expect(convert('_italic _ italic_', 'html')).toBe(
             '<p><i>italic _ italic</i></p>',
         )
     })
 
     test('with just symbol in word in tags', () => {
-        expect(convert('_italic_italic_')).toBe('<p><i>italic_italic</i></p>')
+        expect(convert('_italic_italic_', 'html')).toBe(
+            '<p><i>italic_italic</i></p>',
+        )
     })
 })
 
 describe('Checking monospaced tags:', () => {
     test('with common tags', () => {
-        expect(convert('`monospaced`')).toBe('<p><tt>monospaced</tt></p>')
+        expect(convert('`monospaced`', 'html')).toBe(
+            '<p><tt>monospaced</tt></p>',
+        )
     })
 
     test('with just symbol', () => {
-        expect(convert('`')).toBe('<p>`</p>')
+        expect(convert('`', 'html')).toBe('<p>`</p>')
     })
 
     test('with just symbol in tags', () => {
-        expect(convert('`monospaced ` monospaced`')).toBe(
+        expect(convert('`monospaced ` monospaced`', 'html')).toBe(
             '<p><tt>monospaced ` monospaced</tt></p>',
         )
     })
 
     test('with just symbol in word in tags', () => {
-        expect(convert('`monospaced`monospaced`')).toBe(
+        expect(convert('`monospaced`monospaced`', 'html')).toBe(
             '<p><tt>monospaced`monospaced</tt></p>',
         )
     })
@@ -75,7 +83,7 @@ describe('Checking monospaced tags:', () => {
 
 describe('Checking preformatted tags:', () => {
     test('with another tags', () => {
-        expect(convert('```\r\n**bold** and _italic_\r\n```')).toBe(
+        expect(convert('```\r\n**bold** and _italic_\r\n```', 'html')).toBe(
             '<p><pre>\r\n**bold** and _italic_\r\n</pre></p>',
         )
     })
@@ -83,22 +91,22 @@ describe('Checking preformatted tags:', () => {
 
 describe('Errors', () => {
     test('with nesting tag', () => {
-        expect(() => convert('**_This is invalid_**')).toThrow(
+        expect(() => convert('**_This is invalid_**', 'html')).toThrow(
             STRINGS.errRegExp,
         )
     })
 
     test('with nesting tags', () => {
-        expect(() => convert('**This _is_ invalid**')).toThrow(
+        expect(() => convert('**This _is_ invalid**', 'html')).toThrow(
             STRINGS.errRegExp,
         )
     })
 
     test('with open tag', () => {
-        expect(() => convert('**hello')).toThrow(STRINGS.errRegExp)
+        expect(() => convert('**hello', 'html')).toThrow(STRINGS.errRegExp)
     })
 
     test('with text after preformatted tag', () => {
-        expect(() => convert('```hello')).toThrow(STRINGS.errRegExp)
+        expect(() => convert('```hello', 'html')).toThrow(STRINGS.errRegExp)
     })
 })
